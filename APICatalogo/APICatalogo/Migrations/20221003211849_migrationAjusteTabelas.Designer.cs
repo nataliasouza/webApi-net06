b52f49318019883d9f6ca18200b2850786022e6e
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-10-03 18:28:07 -0300

    Migration - Ajustes nas tabelas

--- APICatalogo/APICatalogo/Migrations/20221003211849_migrationAjusteTabelas.Designer.cs
+++ APICatalogo/APICatalogo/Migrations/20221003211849_migrationAjusteTabelas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using APICatalogo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APICatalogo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221003211849_migrationAjusteTabelas")]
+    partial class migrationAjusteTabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
