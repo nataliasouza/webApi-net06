c59f79d1c3f28aea35f2a1bffd5db1798a1c08cd
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-10-23 21:56:17 -0300

    Ajuste no endpoint

--- APICatalogo/APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/APICatalogo/Controllers/CategoriasController.cs
@@ -33,7 +33,7 @@ namespace APICatalogo.Controllers
             }
         }
 
-        [HttpGet("categorias")]
+        [HttpGet]
         public ActionResult<IEnumerable<Categoria>> GetCategoria()
         {
             try
