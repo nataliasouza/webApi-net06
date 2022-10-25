f5e27eec5a3620d168cd0855ae88ef3c11159ba9
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-10-25 17:23:06 -0300

    Inclusão do Data Annotation

--- APICatalogo/APICatalogo/DTOs/CategoriaDTO.cs
+++ APICatalogo/APICatalogo/DTOs/CategoriaDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace APICatalogo.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs
 {
     public class CategoriaDTO
     {
-     
         public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(80, ErrorMessage = "O nome deve conter entre 5 e 80 caracteres", MinimumLength = 5)]
         public string? Nome { get; set; }
+        [Required(ErrorMessage = "Digite a url da imagem - *Campo Obrigatório*")]
+        [MaxLength(80)]
         public string? ImagemUrl { get; set; }
         public ICollection<ProdutoDTO>? Produtos { get; set; }
     }
--- APICatalogo/APICatalogo/DTOs/ProdutoDTO.cs
+++ APICatalogo/APICatalogo/DTOs/ProdutoDTO.cs
@@ -1,12 +1,26 @@
-﻿namespace APICatalogo.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.DTOs
 {
     public class ProdutoDTO
     {
         public int ProdutoId { get; set; }
-        public string? Nome { get; set; }        
+
+        [Required]
+        [StringLength(50, ErrorMessage = "O nome deve conter entre 5 e 50 caracteres - *Campo Obrigatório*", MinimumLength = 5)]
+        public string? Nome { get; set; }
+        [Required]
+        [StringLength(300, ErrorMessage = "A descrição deve conter entre {2} até {1} caracteres", MinimumLength = 5)]
         public string? Descricao { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
         public decimal Preco { get; set; }
+        [Required(ErrorMessage = "Digite a url da imagem - *Campo Obrigatório*")]
+        [MaxLength(80)]
         public string? ImagemUrl { get; set; }
+        [Range(1, 30, ErrorMessage = "O id da categoria deve estar entre {1} e {2}")]
         public int CategoriaId { get; set; }
 
     }
