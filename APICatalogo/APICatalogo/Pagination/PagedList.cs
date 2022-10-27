c3e5493bb22ae38461285b2d1384b012425cf001
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-10-27 16:53:50 -0300

    Páginação Assíncrona

--- APICatalogo/APICatalogo/Pagination/PagedList.cs
+++ APICatalogo/APICatalogo/Pagination/PagedList.cs
@@ -1,4 +1,6 @@
-﻿namespace APICatalogo.Pagination
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Pagination
 {
     public class PagedList<T> : List<T>
     {
@@ -20,10 +22,10 @@
             AddRange(items);
         }
 
-        public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        public async static Task<PagedList<T>> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = source.Count();
-            var items = source.Skip((pageNumber -1) * pageSize).Take(pageSize).ToList();
+            var items = await source.Skip((pageNumber -1) * pageSize).Take(pageSize).ToListAsync();
 
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
