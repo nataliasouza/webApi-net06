5b6f9baa7d645578964fff2959a44b883603d8c5
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-10-28 11:21:24 -0300

    Registro/Login/Token - AutorizaController

---
+++ APICatalogo/APICatalogo/Controllers/AutorizaController.cs
@@ -0,0 +1,117 @@
+﻿using APICatalogo.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController] 
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _config;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : "
+                + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("registraUsuario")]
+
+        public async Task<ActionResult> RegistraUsuario([FromBody]UsuarioDTO usuario)
+        {
+            if (usuario is null)
+            {
+                return BadRequest("Digite as informações necessárias - *Campo Obrigatório*");
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = usuario.Email,
+                Email = usuario.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuario.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok(GerarToken(usuario));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo)
+        {
+            if (userInfo is null)
+            {
+                return BadRequest("Verifique as informações preenchidas");
+            }
+
+            //verifica as credenciais do usuário e retorna um valor
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email,
+                userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if(result.Succeeded)
+            {
+                return Ok(GerarToken(userInfo));
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login Inválido!");
+                return BadRequest(ModelState);
+            }
+        }
+
+        private UsuarioToken GerarToken(UsuarioDTO userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userInfo.Email),
+                new Claim("Gerar","Token"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+
+            var credenciais = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expirarToken = _config["TokenConfiguration:ExpireHours"];
+            var expiracao = DateTime.UtcNow.AddHours(double.Parse(expirarToken));
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _config["TokenConfiguration:Issuer"],
+                audience: _config["TokenConfiguration:Audience"],
+                claims: claims,
+                expires: expiracao,
+                signingCredentials: credenciais);
+
+            return new UsuarioToken() { 
+                Authenticated = true,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiracao,
+                Message = "Token JWT OK - Success"
+            };
+        }
+    }
+}
