3f00e3d6fb774224cb9dfa589fd8d65f996a6a19
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-10-31 17:37:16 -0300

    Configuração para utilizar o versionamento no Swagger

---
+++ APICatalogo/APICatalogo/ConfigureSwaggerOptions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    readonly IApiVersionDescriptionProvider provider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
+    /// </summary>
+    /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+    /// <inheritdoc />
+    public void Configure(SwaggerGenOptions options)
+    {
+        // add a swagger document for each discovered API version
+        // note: you might choose to skip or document deprecated API versions differently
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+        }
+    }
+
+    static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = "APICatalogo",
+            Version = description.ApiVersion.ToString(),
+            Description = "Exemplo com a API de Produtos, Swashbuckle e API versioning.",
+            Contact = new OpenApiContact() { Name = "Natalia Souza", Url = new Uri("https://github.com/nataliasouza") }           
+        };
+
+        if (description.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated.";
+        }
+
+        return info;
+    }
+}
+
