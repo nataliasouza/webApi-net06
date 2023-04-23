73232a2b7f87b2268fe5e9eb794a13c3b79e3c73
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-22 23:35:34 -0300

    Inclusão das classes para utilizar tracing

---
+++ APICatalogo/Sample.OpenTelemetry.WebApi.Core/Configurations/AppSetting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.OpenTelemetry.WebApi.Core.Configurations
+{
+    public class AppSetting
+    {
+        public DistributedTracingOptions DistributedTracing { get; set; }
+    }
+
+    public class DistributedTracingOptions
+    {
+        public bool IsEnabled { get; set; }
+        public JaegerOptions Jaeger { get; set; }
+    }
+
+    public class JaegerOptions
+    {
+        public string ServiceName { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
+
---
+++ APICatalogo/Sample.OpenTelemetry.WebApi.Core/Extensions/OpenTelemetryExtension.cs
@@ -0,0 +1,84 @@
+﻿using MassTransit.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Sample.OpenTelemetry.WebApi.Core.Configurations;
+using System.Diagnostics;
+
+namespace Sample.OpenTelemetry.WebApi.Core.Extensions
+{
+	public static class OpenTelemetryExtension
+	{
+		public static ActivitySource ActivitySource;
+		public static void AddOpenTelemetry(this WebApplicationBuilder builder, AppSetting appSettings)
+		{
+			void ConfigureResource(ResourceBuilder r) => r
+				.AddService(serviceName: appSettings?.DistributedTracing?.Jaeger?.ServiceName ?? string.Empty,
+				serviceVersion: typeof(OpenTelemetryExtension).Assembly.GetName().Version?.ToString() ?? "unknown",
+				serviceInstanceId: Environment.MachineName)
+				.AddTelemetrySdk()
+				.AddEnvironmentVariableDetector()
+				.AddAttributes(new Dictionary<string, object>
+				{
+					["environment.name"] = "development",
+					["team.name"] = "backend"
+				});
+
+			builder.Services.AddOpenTelemetry()
+				.ConfigureResource(ConfigureResource)
+				.WithTracing(p =>
+				{
+					p.AddSource(DiagnosticHeaders.DefaultListenerName, appSettings.DistributedTracing.Jaeger.ServiceName, "MassTransit")
+						.AddAspNetCoreInstrumentation(p =>
+						{
+							p.RecordException = true;
+						})
+						.AddHttpClientInstrumentation(p =>
+						{
+							p.RecordException = true;
+						})
+						.AddSqlClientInstrumentation(p =>
+						{
+							p.SetDbStatementForText = true;
+							p.EnableConnectionLevelAttributes = true;
+							p.RecordException = true;
+						})
+						.AddEntityFrameworkCoreInstrumentation(p =>
+						{
+							p.SetDbStatementForText = true;
+						})
+						.AddMassTransitInstrumentation()
+						.SetSampler(new AlwaysOnSampler())
+						.AddJaegerExporter(p =>
+						{
+							p.AgentHost = appSettings?.DistributedTracing?.Jaeger?.Host;
+							p.AgentPort = appSettings?.DistributedTracing?.Jaeger?.Port ?? 0;
+						});
+				});
+
+			builder.Services.AddLogging(build =>
+			{
+				build.SetMinimumLevel(LogLevel.Debug);
+				build.AddOpenTelemetry(options =>
+				{
+					options.AddConsoleExporter().SetResourceBuilder(ResourceBuilder.CreateDefault()
+						.AddService(appSettings?.DistributedTracing?.Jaeger?.ServiceName ?? string.Empty))
+						.AddProcessor(new ActivityEventExtensions()).IncludeScopes = true;
+				});
+			});
+
+			builder.Services.Configure<AspNetCoreInstrumentationOptions>(options => options.RecordException = true);
+			builder.Services.Configure<OpenTelemetryLoggerOptions>(opt =>
+			{
+				opt.IncludeScopes = true;
+				opt.ParseStateValues = true;
+				opt.IncludeFormattedMessage = true;
+			});
+			ActivitySource = new ActivitySource(appSettings?.DistributedTracing?.Jaeger?.ServiceName ?? string.Empty);
+		}
+	}
+}
