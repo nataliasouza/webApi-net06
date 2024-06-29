8400326ba05cff48b9ce91de42a6db4f025feef3
Natalia Souza <nataliasouza.ti@hotmail.com>
2024-06-29 18:35:22 -0300

    chore: Remove nulidade de DbSet em AppDbContext

--- APICatalogo/APICatalogo/Data/AppDbContext.cs
+++ APICatalogo/APICatalogo/Data/AppDbContext.cs
@@ -4,7 +4,7 @@
     {
         public AppDbContext(DbContextOptions<AppDbContext> options ) : base( options )
         {}
-        public DbSet<Categoria>? Categorias { get; set; }
-        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
