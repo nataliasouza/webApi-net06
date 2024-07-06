28ccafb5268a08fe4c39fb34265e93ebb21e86e9
Natalia Souza <nataliasouza.ti@hotmail.com>
2024-07-05 22:51:48 -0300

    chore: Inclui os cenários e gerei os stepDefinitions (testes)

---
+++ APICatalogo/BDD/Categoria_AdicionarCategoriaStepDefinitions.cs
@@ -0,0 +1,128 @@
+using TechTalk.SpecFlow;
+
+namespace BDD
+{
+    [Binding]
+    public class Categoria_AdicionarCategoriaStepDefinitions
+    {
+        [Given(@"que estou na pagina de cadastro categorias")]
+        public void GivenQueEstouNaPaginaDeCadastroCategorias()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"quero adicionar uma nova categoria")]
+        public void GivenQueroAdicionarUmaNovaCategoria()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"também quero adicionar um ou mais produtos a essa categoria")]
+        public void GivenTambemQueroAdicionarUmOuMaisProdutosAEssaCategoria()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [When(@"eu prencher a categoria com os dados corretos e com os produtos corretos")]
+        public void WhenEuPrencherACategoriaComOsDadosCorretosEComOsProdutosCorretos()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"a categoria de produtos deve ser cadastrada com sucesso")]
+        public void ThenACategoriaDeProdutosDeveSerCadastradaComSucesso()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"quero adicionar uma nova categoria com produtos já existentes")]
+        public void GivenQueroAdicionarUmaNovaCategoriaComProdutosJaExistentes()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [When(@"eu prencher a categoria com os dados corretos e com os produtos ja existentes")]
+        public void WhenEuPrencherACategoriaComOsDadosCorretosEComOsProdutosJaExistentes()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"deve ser exibida uma mensagem de erro informando que os produtos já estão cadastrados na categoria")]
+        public void ThenDeveSerExibidaUmaMensagemDeErroInformandoQueOsProdutosJaEstaoCadastradosNaCategoria()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"quero adicionar uma nova categoria sem produtos")]
+        public void GivenQueroAdicionarUmaNovaCategoriaSemProdutos()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [When(@"eu prencher a categoria com os dados corretos")]
+        public void WhenEuPrencherACategoriaComOsDadosCorretos()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"a categoria deve ser cadastrada com sucesso")]
+        public void ThenACategoriaDeveSerCadastradaComSucesso()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"deve ser exibida uma mensagem de erro informando que a categoria já existe")]
+        public void ThenDeveSerExibidaUmaMensagemDeErroInformandoQueACategoriaJaExiste()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
